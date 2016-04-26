013e6b040ddabcc55a9a7619b5a822d0e6b94ba9
Rado Ondas <ondas.radovan@gmail.com>
2016-04-26 23:06:28 +0200

    Change default template for ES-2 and not ES-5

--- update.sh
+++ update.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-sh ../../elastic/beats/libbeat/scripts/update.sh jmxproxybeat . ../../elastic/beats/libbeat
+sh ../../elastic/beats/libbeat/scripts-3.4/update.sh jmxproxybeat . ../../elastic/beats/libbeat
+
+# python3 ../../elastic/beats/dev-tools/export_dashboards-fix.py --url http://127.0.0.1:9555 --beat jmxproxybeat --index jmxproxybeat-* --dir etc/kibana5/
