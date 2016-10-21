71bea6005f4f5da4f541dba24eb48102ddc4ceb4
radoondas <ondas.radovan@gmail.com>
2016-10-21 09:41:14 +0200

    Remove file

--- update.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-sh ../../elastic/beats/libbeat/scripts-3.4/update.sh jmxproxybeat . ../../elastic/beats/libbeat
-
-# python3 ../../elastic/beats/dev-tools/export_dashboards-fix.py --url http://127.0.0.1:9555 --beat jmxproxybeat --index jmxproxybeat-* --dir etc/kibana5/
